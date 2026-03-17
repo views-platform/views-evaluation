db1e087e0f6a2009797f852f2eed9369f7a7ce19
xiaolongsun <95378566+xiaolong0728@users.noreply.github.com>
2026-03-17 13:05:22 +0100

    init file

--- views_evaluation/manager/__init__.py
+++ views_evaluation/manager/__init__.py
@@ -1,7 +1,9 @@
 from .base import EvaluationManager
 from .stepshifter import StepshifterEvaluationManager
+from .impact import ImpactEvaluationManager
 
 __all__ = [
     "EvaluationManager",
     "StepshifterEvaluationManager",
+    "ImpactEvaluationManager",
 ]
